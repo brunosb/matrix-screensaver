d64311f55ffcd6f3f9b8ae375575a4677756ee40
Bruno Barbosa <barbosa.developer@gmail.com>
2021-10-31 15:24:56 -0300

    adicionando arquivos faltantes do ultimo commit

--- src/electron/CreateWindow.js
+++ src/electron/CreateWindow.js
@@ -2,32 +2,41 @@ const { BrowserWindow, screen } = require('electron')
 const path = require('path')
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    fullscreen: true,
-    resizable: false,
-    webPreferences: {
-      preload: path.join(__dirname, '../', '../', 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+  let browsers = [];
+  const displays = screen.getAllDisplays()
 
-  //win.webContents.openDevTools()
+  displays.forEach((display) => {
+    const win = new BrowserWindow({
+      x: display.bounds.x,
+      y: display.bounds.y,
+      fullscreen: true,
+      resizable: true,
+      webPreferences: {
+        preload: path.join(__dirname, '../', '../', 'preload.js'),
+        nodeIntegration: true,
+        contextIsolation: false
+      }
+    })
+  
+    //win.webContents.openDevTools()
+  
+    win.loadFile('index.html')
+    win.setAlwaysOnTop(true, 'screen-saver')
+    win.setVisibleOnAllWorkspaces(true)
+    win.maximize()
+    win.setMenu(null)
+  
+    win.on('closed', function() {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null;
+    });
+  
+    browsers.push(win);
+  });  
 
-  win.loadFile('index.html')
-
-  win.setMenu(null);
-
-  win.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-
-  return win
+  return browsers;
 }
 
 module.exports = createWindow();
